b47c355be9f6208e8188df7694a616962f4a95c6
oheiheiheiheihei <294797392@qq.com>
2020-08-03 13:09:53 +0800

    add file
    add file

---
+++ Sources/WPFClient/DemoEnumeration.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFToolkit.MarkupExtensions;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// DemoEnumeration类
+    /// </summary>
+    public enum DemoEnumeration
+    {
+        [EnumMember("第一个")]
+        One,
+
+        [EnumMember("第二个")]
+        Two,
+
+        [EnumMember("第三个")]
+        Three
+    }
+}
