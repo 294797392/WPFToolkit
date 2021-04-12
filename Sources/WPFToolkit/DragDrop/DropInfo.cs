926406f737d3fef845229418a835f31d4e107b84
张宇锋 <294797392@qq.com>
2021-04-12 11:26:18 +0800

    dragdop

---
+++ Sources/WPFToolkit/DragDrop/DropInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFToolkit.DragDrop
+{
+    public class DropInfo
+    {
+        public DropInfo()
+        {
+        }
+
+        public DropInfo(object sender, DragEventArgs e, DragInfo dragInfo, string dataFormat)
+        {
+            Data = (e.Data.GetDataPresent(dataFormat)) ? e.Data.GetData(dataFormat) : e.Data;
+            this.DragEventArgs = e;
+            DragInfo = dragInfo;
+
+            VisualTarget = sender as UIElement;
+
+            if (sender is ItemsControl)
+            {
+                ItemsControl itemsControl = (ItemsControl)sender;
+                UIElement item = itemsControl.GetItemContainerAt(e.GetPosition(itemsControl));
+
+                VisualTargetOrientation = itemsControl.GetItemsPanelOrientation();
+
+                if (item != null)
+                {
+                    ItemsControl itemParent = ItemsControl.ItemsControlFromItemContainer(item);
+
+                    InsertIndex = itemParent.ItemContainerGenerator.IndexFromContainer(item);
+                    TargetCollection = itemParent.ItemsSource ?? itemParent.Items;
+                    TargetItem = itemParent.ItemContainerGenerator.ItemFromContainer(item);
+                    VisualTargetItem = item;
+
+                    if (VisualTargetOrientation == Orientation.Vertical)
+                    {
+                        if (e.GetPosition(item).Y > item.RenderSize.Height / 2) InsertIndex++;
+                    }
+                    else
+                    {
+                        if (e.GetPosition(item).X > item.RenderSize.Width / 2) InsertIndex++;
+                    }
+                }
+                else
+                {
+                    TargetCollection = itemsControl.ItemsSource ?? itemsControl.Items;
+                    InsertIndex = itemsControl.Items.Count;
+                }
+            }
+        }
+
+
+        public object Data { get; internal set; }
+        public DragInfo DragInfo { get; internal set; }
+        public Type DropTargetAdorner { get; set; }
+        public DragDropEffects Effects { get; set; }
+        public int InsertIndex { get; private set; }
+        public IEnumerable TargetCollection { get; internal set; }
+        public object TargetItem { get; internal set; }
+        public UIElement VisualTarget { get; internal set; }
+        public UIElement VisualTargetItem { get; internal set; }
+        public Orientation VisualTargetOrientation { get; internal set; }
+        public Point DropPosition { get; set; }
+
+        public DragEventArgs DragEventArgs { get; internal set; }
+    }
+}
---
+++ Sources/WPFToolkit/DragDrop/WinformDragDropHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace WPFToolkit.DragDrop
+{
+    public class WinformDragDropHelper
+    {
+        public static void RegisterDropHandler(System.Windows.Forms.Control control)
+        {
+            if (control is IDropHandler)
+            {
+                control.AllowDrop = true;
+                control.DragDrop += FormControl_DragDrop;
+                control.DragEnter += FormControl_DragEnter;
+                control.DragLeave += FormControl_DragLeave;
+                control.DragOver += FormControl_DragOver;
+            }
+        }
+
+        public static void UnRegisterDropHandler(System.Windows.Forms.Control control)
+        {
+            control.AllowDrop = false;
+            control.DragDrop -= FormControl_DragDrop;
+            control.DragEnter -= FormControl_DragEnter;
+            control.DragLeave -= FormControl_DragLeave;
+            control.DragOver -= FormControl_DragOver;
+        }
+
+        static void FormControl_DragOver(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            IDropHandler dropTarget = sender as IDropHandler;
+            if (dropTarget != null)
+            {
+                DropInfo dropInfo = new DropInfo();
+                if (DragDrop.m_DragInfo != null)
+                {
+                    dropInfo.Data = DragDrop.m_DragInfo.Data;
+                }
+
+                dropTarget.OnDragOver(dropInfo);
+
+                if (dropInfo.Effects != DragDropEffects.None)
+                {
+                    e.Effect = Convert(dropInfo.Effects);
+                }
+            }
+        }
+
+        static void FormControl_DragLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        static void FormControl_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            FormControl_DragOver(sender, e);
+        }
+
+        static void FormControl_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            IDropHandler dropTarget = sender as IDropHandler;
+            if (dropTarget != null)
+            {
+                DropInfo dropInfo = new DropInfo();
+                if (DragDrop.m_DragInfo != null)
+                {
+                    dropInfo.Data = DragDrop.m_DragInfo.Data;
+                }
+
+                dropTarget.OnDrop(dropInfo);
+            }
+        }
+
+        static System.Windows.Forms.DragDropEffects Convert(DragDropEffects effect)
+        {
+            switch (effect)
+            {
+                case DragDropEffects.All:
+                    return System.Windows.Forms.DragDropEffects.All;
+
+                case DragDropEffects.Copy:
+                    return System.Windows.Forms.DragDropEffects.Copy;
+
+                case DragDropEffects.Link:
+                    return System.Windows.Forms.DragDropEffects.Link;
+
+                case DragDropEffects.Move:
+                    return System.Windows.Forms.DragDropEffects.Move;
+
+                case DragDropEffects.Scroll:
+                    return System.Windows.Forms.DragDropEffects.Scroll;
+
+                case DragDropEffects.None:
+                default:
+                    return System.Windows.Forms.DragDropEffects.None;
+            }
+        }
+
+    }
+}
