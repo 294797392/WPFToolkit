5c942f3f89548fc3685aab8901637dab6249cfc6
张宇锋 <294797392@qq.com>
2021-05-13 15:17:00 +0800

    .

---
+++ Sources/WPFToolkit/Business.Controls/BusinessTransfer.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFToolkit.Business.Controls
+{
+    /// <summary>
+    /// 穿梭框控件
+    /// </summary>
+    public partial class BusinessTransfer : UserControl
+    {
+        public BusinessTransfer()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Sources/WPFToolkit/Controls/Templates/Templates.cs
+++ Sources/WPFToolkit/Controls/Templates/Templates.cs
@@ -16,6 +16,8 @@ namespace WPFToolkit.Controls
 
         public static Style KButtonStyle { get; private set; }
 
+        public static Style KListBoxStyle { get; private set; }
+
         static Templates()
         {
             ResourceDictionary resource = new ResourceDictionary()
@@ -23,10 +25,12 @@ namespace WPFToolkit.Controls
                 Source = new Uri(ResourceURI)
             };
 
+            // 把资源文件加到应用程序里
             Application.Current.Resources.MergedDictionaries.Add(resource);
 
             KComboBoxStyle = resource["KComboBoxStyle"] as Style;
             KButtonStyle = resource["KButtonStyle"] as Style;
+            KListBoxStyle = resource["KListBoxStyle"] as Style;
         }
     }
 }
