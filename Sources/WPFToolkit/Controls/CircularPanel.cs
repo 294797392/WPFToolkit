ef89953ec5d1f20f0f2ae6e5195f00d010fa492e
zhangyufeng <zhangyf3@hyc.cn>
2022-12-15 20:12:19 +0800

    .

---
+++ Sources/WPFToolkit/Controls/CircularPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFToolkit.Controls
+{
+    public class CircularPanel : Panel
+    {
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
